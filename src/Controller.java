cfe4a9ed4ef8cf053d70a160b055755f2a6210a3
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-22 00:08:33 +0200

    MVC

---
+++ src/Controller.java
@@ -0,0 +1,55 @@
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
+public class Controller {
+
+    public void initController(){
+        View view = new View();
+        Model model = new Model();
+
+        while(true){
+            view.printMenuData();
+            Integer command = view.readCommand();
+            if(command.equals(5)){
+                break;
+            }
+            else if(command.equals(1)){
+                view.printIncomingString("Enter data to add");
+                model.create(view.readStringData(),view.readIntData());
+                view.printIncomingString("data was added");
+            }
+            else if(command.equals(2)){
+                view.printIncomingString("Enter data to find");
+                Data data = model.find(view.readStringData());
+
+                if(data != null){
+                    view.printData(data);
+                }else {
+                    view.printIncomingString("Noting to show");
+                }
+
+            }
+            else if(command.equals(3)){
+                view.printIncomingString("Enter data to update: ");
+                view.printIncomingString("Enter old name: ");
+                Data newData = model.update(view.readStringData(),view.readStringData(),view.readIntData());
+                view.printIncomingString("Result of updating: ");
+                if(newData != null){
+                    view.printData(newData);
+                }else{
+                    view.printIncomingString("Nothing to update");
+                }
+            }
+            else if(command.equals(4)){
+                view.printIncomingString("Enter data to delete");
+                int index = model.delete(view.readStringData());
+                if(index != -1){
+                    view.printIncomingString("Data in index: " + index + "was deleted");
+                }else {
+                    view.printIncomingString("Nothing to delete");
+                }
+            }else{
+                view.printIncomingString("wrong command");
+            }
+        }
+    }
+}
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,17 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainApp {
 
     public static void main(String...args){
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("ENTER STRING");
-        String incomingString = scanner.nextLine();
-        System.out.println("Your line is: " + incomingString);
-
-
-
+        Controller controller = new Controller();
+        controller.initController();
     }
 
 
