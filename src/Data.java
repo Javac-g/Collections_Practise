ad4b452a4bea48e653b649dd605e831af4144c0f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-21 21:48:08 +0200

    Second Ver.

---
+++ src/Data.java
@@ -0,0 +1,31 @@
+public class Data {
+
+    private String name;
+
+    private Integer id;
+
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public Integer getId() {
+
+        return id;
+
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+
+    }
+}
