81cfe61f557d0963db49f8cf76963a4ee512bf7e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-21 20:25:19 +0200

    First

---
+++ src/MainApp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String...args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ENTER STRING");
+        String incomingString = scanner.nextLine();
+        System.out.println("Your line is: " + incomingString);
+
+
+
+    }
+
+
+}
