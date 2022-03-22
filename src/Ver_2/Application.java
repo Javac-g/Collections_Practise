b430a32b78950b5199184011fc6a4a64d943bade
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-22 18:21:44 +0200

    App start created

--- src/Ver_2/Application.java
+++ src/Ver_2/Application.java
@@ -1,4 +1,12 @@
 package Ver_2;
+
+import Ver_2.Control.Controller;
+
 //Point of starting application.
 public class Application {
+    public static void main(String...args){
+
+        Controller controller = new Controller();
+        controller.initController();
+    }
 }
