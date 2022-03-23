4c264de3c1d0655bcbc1332afe51c471dae27eb4
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-23 14:32:31 +0200

    bug fix

--- src/Ver_2/Application.java
+++ src/Ver_2/Application.java
@@ -2,9 +2,11 @@ package Ver_2;
 
 import Ver_2.Control.Controller;
 
+import java.io.IOException;
+
 //Point of starting application.
 public class Application {
-    public static void main(String...args){
+    public static void main(String...args) throws IOException {
 
         Controller controller = new Controller();
         controller.initController();
