653bd6afd6f2e409fcf8d298c242537d3d12937e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-28 22:25:32 +0300

    Model create

--- src/Ver_4/Model/Model.java
+++ src/Ver_4/Model/Model.java
@@ -20,5 +20,43 @@ public class Model {
         System.out.println("Data add successful");
     }
 
+    public Data Read(String name){
+
+        for (Data search:database) {
+            if(search.getName().equals(name)){
+
+                return search;
+
+            }
+
+
+        }
+        return null;
+
+    }
+
+    public Data Update(String name,String newName,String newPosition,Integer newSalary ){
+        Data data = Read(name);
+        if(data != null){
+            data.setName(newName);
+            data.setPosition(newPosition);
+            data.setSalary(newSalary);
+        }
+        return null;
+    }
+    public Integer Delete(String deleteName){
+        int indexToDelete = -1;
+        for (int i = 0; i < database.size();i++){
+            if(database.get(i).getName().equals(deleteName)){
+                indexToDelete = i;
+                break;
+            }
+        }
+        if(indexToDelete != -1){
+            database.remove(indexToDelete);
+        }
+        return indexToDelete;
+    }
+
 
 }
