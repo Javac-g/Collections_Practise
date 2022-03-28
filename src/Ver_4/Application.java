0cff672b2ee19f1e0c9d0aee888350e11ab87f4c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-28 23:50:27 +0300

    App

--- src/Ver_4/Application.java
+++ src/Ver_4/Application.java
@@ -1,10 +1,12 @@
 package Ver_4;
 
+import Ver_4.Controller.Controller;
+
 public class Application {
 
     public static void main(String...args){
 
-
-
+        Controller controller = new Controller();
+        controller.initController();
     }
 }
