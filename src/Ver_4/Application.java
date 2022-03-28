f037556476997ca3c9ef71c04ff8b2797587c5e9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-03-28 21:11:19 +0300

    User data created

--- src/Ver_4/Application.java
+++ src/Ver_4/Application.java
@@ -1,4 +1,10 @@
 package Ver_4;
 
 public class Application {
+
+    public static void main(String...args){
+
+
+
+    }
 }
---
+++ src/Ver_4/User/Data.java
@@ -0,0 +1,32 @@
+package Ver_4.User;
+
+public class Data {
+
+    String name;
+    String position;
+    Integer salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
