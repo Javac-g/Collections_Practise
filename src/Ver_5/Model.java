98fc05d5bbcd40122cf9c144c5bf3a38241bcfb5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-07 22:14:20 +0300

    model done

--- src/Ver_5/Model.java
+++ src/Ver_5/Model.java
@@ -16,6 +16,40 @@ public class Model {
 
     }
 
-    public Data find person
+    public Data find_person(String name){
+
+        for(Data search: datalist){
+            if(search.getName().equals(name)){
+                return search;
+            }
+        }
+        return null;
+    }
+
+    public Data update_person(String name,String newName,String newPosition,Integer newSalary){
+        Data update = find_person(name);
+        if (update != null){
+            update.setName(newName);
+            update.setPosition(newPosition);
+            update.setSalary(newSalary);
+            return update;
+        }
+        return null;
+    }
+
+    public int delete_person(String name){
+        int index = -1;
+        for(int i = 0 ; i >= datalist.size();i++){
+            if(datalist.get(i).getName().equals(name)){
+                index = i;
+                break;
+            }
+        }
+        if(index != -1){
+            datalist.remove(index);
+        }
+        return index;
+
+    }
 
 }
