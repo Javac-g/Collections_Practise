6cf726dee160931e3893f2a3d66014e3e1d44ecc
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-07 21:46:13 +0300

    data set

--- src/Ver_5/Data.java
+++ src/Ver_5/Data.java
@@ -1,4 +1,32 @@
 package Ver_5;
 
 public class Data {
+
+    private String name;
+    private String position;
+    private Integer salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
 }
