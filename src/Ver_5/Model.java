b1c1f95082b15c29f73f96141b57fcda7dbfe105
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-07 22:04:35 +0300

    Model

--- src/Ver_5/Model.java
+++ src/Ver_5/Model.java
@@ -1,4 +1,21 @@
 package Ver_5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+    Data data = new Data();
+    List<Data> datalist = new ArrayList<>();
+
+    public void create_person(String name,String position,Integer Salary){
+
+        data.setName(name);
+        data.setPosition(position);
+        data.setSalary(Salary);
+        datalist.add(data);
+
+    }
+
+    public Data find person
+
 }
