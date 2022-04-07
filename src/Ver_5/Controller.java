d852d159f726eeb45ac776d55cfe6169759072dd
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-07 22:18:55 +0300

    create

--- src/Ver_5/Controller.java
+++ src/Ver_5/Controller.java
@@ -1,4 +1,29 @@
 package Ver_5;
 
 public class Controller {
+    public void initController(){
+
+        Model model = new Model();
+        View view = new View();
+
+        while (true){
+            view.printMenu();
+            Integer command = view.readCommand();
+
+            if(command == 5){
+                break;
+            }
+            else if(command == 1){
+                view.printMessage("Create new person");
+                model.create_person(view.addName(),view.addPosition(),view.addSalary());
+                view.printMessage("Person created! ");
+            }
+
+        }
+
+
+
+
+
+    }
 }
