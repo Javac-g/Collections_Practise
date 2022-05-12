9a7409435b89346a7e5d57004afaeaaf39163916
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-12 23:09:15 +0300

    three

--- src/Ver_6/Model.java
+++ src/Ver_6/Model.java
@@ -1,4 +1,28 @@
 package Ver_6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+
+    List<Data> dataList = new ArrayList<>();
+
+    public void create(String name, Integer salary){
+
+        Data person = new Data();
+
+        person.setName(name);
+
+        person.setSalary(salary);
+
+        dataList.add(person);
+
+    }
+
+
+
+
+
+
+
 }
