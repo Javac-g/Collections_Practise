1f8bd2d13b1e30c655b7576e2ef99aa908433789
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-27 22:46:24 +0200

    Stack push

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -1,4 +1,7 @@
 package Collections;
 
 public class Main {
+    public static void main(String...args){
+
+    }
 }
---
+++ src/Collections/Stack.java
@@ -0,0 +1,22 @@
+package Collections;
+
+public class Stack {
+    private int last,size;
+    private int[] stck;
+
+    public Stack(int size) {
+        stck = new int[size];
+        last = -1;
+    }
+    public void push(int item){
+        if (last >= stck.length){
+            System.out.println("Stack is full");
+            int[] temp = new int[stck.length*2];
+            for (int i = 0;i < stck.length;i++){
+                temp[i] = stck[i];
+            }
+            stck = temp;
+        }
+        stck[++last] = item;
+    }
+}
